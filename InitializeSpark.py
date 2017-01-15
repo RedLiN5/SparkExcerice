a42286438a1b52eb69691064dff262f03b124483
Leslie_Yan <leslieyan0621@gmail.com>
2017-01-15 16:18:02 +0800

    RDD foundation

--- InitializeSpark.py
+++ InitializeSpark.py
@@ -2,3 +2,13 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster('local').setAppName('My App')
 sc = SparkContext(conf = conf)
+
+lines = sc.textFile('README_example.md')
+
+pythonLines = lines.filter(lambda line: 'Python' in line)
+print('First element:', pythonLines.first())
+
+# Persist RDD in memory
+pythonLines.persist()
+
+print 'Count:', pythonLines.count()
